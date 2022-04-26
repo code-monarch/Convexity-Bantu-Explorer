1c611602561cb8f5fc06d7b377267d20f3d85564
Joel <78717119+code-monarch@users.noreply.github.com>
2022-04-26 08:58:02 +0100

    Correct Empty components export for Solana and BNB

--- pages/bnb/wallet/[id].jsx
+++ pages/bnb/wallet/[id].jsx
@@ -1,4 +1,4 @@
-// import Header from "../../../components/dashboard/Header";
+import Header from "../../../components/dashboard/Header";
 // import In from "../../../components/transaction/In";
 // import Out from "../../../components/transaction/Out";
 
@@ -10,12 +10,12 @@
 // import { useGetBalanceQuery } from "../../../services/balanceApi";
 // import { useDispatch } from "react-redux";
 
-// const style = {
-//   container: `w-[80vw] bg-[#fff] ml-[20vw]`,
-//   main: `bg-white w-[100%] h-full overflow-auto flex flex-row justify-around rounded mt-[80px]`,
-// };
+const style = {
+  container: `w-[80vw] bg-[#fff] ml-[20vw]`,
+  main: `bg-white w-[100%] h-full overflow-auto flex flex-row justify-around rounded mt-[80px]`,
+};
 
-// const Wallet = () => {
+const Wallet = () => {
 //   const dispatch = useDispatch();
 
 //   // INSTANTIATE NEXT JS useRouter
@@ -28,8 +28,8 @@
 //   const { data, isFetching } = useGetPaymentsQuery(id);
 //   const payments = data?._embedded?.records;
 
-//   // GET WALLET BALANCE FROM balanceApi, PASS WALLET KEY TO THE useGetBalanceQuery RTK hook. THE balanceApi FETCHES THE WALLET BALANCE FROM BANTU API. THE RETURNED DATA IS DESTRUCTURED FROM THE useGetBalanceQuery hook
-//   // DATA FETCHED IS USED AS THE ALIAS BALANCE TO PREVENT REDEFINED ERROR
+  // GET WALLET BALANCE FROM balanceApi, PASS WALLET KEY TO THE useGetBalanceQuery RTK hook. THE balanceApi FETCHES THE WALLET BALANCE FROM BANTU API. THE RETURNED DATA IS DESTRUCTURED FROM THE useGetBalanceQuery hook
+  // DATA FETCHED IS USED AS THE ALIAS BALANCE TO PREVENT REDEFINED ERROR
 //   const { data: balance } = useGetBalanceQuery(id);
 //   console.log(balance);
 //   // DISPATCH getWalletBalance Action WITH THE FETCHED BALANCE TO BE STORED IN THE BALANCE STATE
@@ -40,7 +40,7 @@
 //     })
 //   );
 
-//   // PAYMENTS IN
+  // PAYMENTS IN
 //   const paymentIn = payments?.filter(
 //     (payment) => payment.from !== id && payment.type == "payment"
 //   );
@@ -53,15 +53,15 @@
 //       payment.asset_type == "create_account"
 //   );
 
-//   return (
-//     <div className={style.container}>
-//       <Header />
-//       <main className={style.main}>
-//         <In paymentIn={paymentIn} />
-//         <Out paymentOut={paymentOut} />
-//       </main>
-//     </div>
-//   );
-// };
+  return (
+    <div className={style.container}>
+      <Header />
+      {/* <main className={style.main}>
+        <In paymentIn={paymentIn} />
+        <Out paymentOut={paymentOut} />
+      </main> */}
+    </div>
+  );
+};
 
-// export default Wallet;
+export default Wallet;
