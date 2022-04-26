54ce0cafe557c130e9425431468ce40d2122f840
Joel <78717119+code-monarch@users.noreply.github.com>
2022-04-26 09:02:06 +0100

    Update [id].jsx

--- pages/tron/wallet/[id].jsx
+++ pages/tron/wallet/[id].jsx
@@ -1,71 +1,71 @@
-import Header from "../../../components/dashboard/Header";
-import TransactionsPageLayout from "../../../components/USDT/TransactionsPageLayout";
-import In from "../../../components/USDT/transaction/In";
-import Out from "../../../components/USDT/transaction/Out";
+// import Header from "../../../components/dashboard/Header";
+// import TransactionsPageLayout from "../../../components/USDT/TransactionsPageLayout";
+// import In from "../../../components/USDT/transaction/In";
+// import Out from "../../../components/USDT/transaction/Out";
 
-import { getWalletBalance } from "../../../app/usdt/usdtbalanceSlice";
+// import { getWalletBalance } from "../../../app/usdt/usdtbalanceSlice";
 
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 
-import { useGetPaymentsQuery } from "../../../services/usdt/usdtpaymentsApi";
-import { useGetBalanceQuery } from "../../../services/usdt/usdtbalanceApi";
-import { useDispatch } from "react-redux";
+// import { useGetPaymentsQuery } from "../../../services/usdt/usdtpaymentsApi";
+// import { useGetBalanceQuery } from "../../../services/usdt/usdtbalanceApi";
+// import { useDispatch } from "react-redux";
 
-const style = {
-  main: `bg-white w-[80vw] h-[100%] flex flex-row justify-around rounded mt-[80px]`,
-};
+// const style = {
+//   main: `bg-white w-[80vw] h-[100%] flex flex-row justify-around rounded mt-[80px]`,
+// };
 
-const Wallet = () => {
-  const dispatch = useDispatch();
+// const Wallet = () => {
+//   const dispatch = useDispatch();
 
-  // INSTANTIATE NEXT JS useRouter
-  const router = useRouter();
+//   // INSTANTIATE NEXT JS useRouter
+//   const router = useRouter();
 
-  // STORE ROUTE QUERY WHICH IS THE WALLET KEY IN A VARIABLE
-  const { id } = router.query;
-  console.log(router.query);
+//   // STORE ROUTE QUERY WHICH IS THE WALLET KEY IN A VARIABLE
+//   const { id } = router.query;
+//   console.log(router.query);
 
-  const { data, isFetching } = useGetPaymentsQuery(id);
-  const payments = data?._embedded?.records;
+//   const { data, isFetching } = useGetPaymentsQuery(id);
+//   const payments = data?._embedded?.records;
 
-  // GET WALLET BALANCE FROM balanceApi, PASS WALLET KEY TO THE useGetBalanceQuery RTK hook. THE balanceApi FETCHES THE WALLET BALANCE FROM BANTU API. THE RETURNED DATA IS DESTRUCTURED FROM THE useGetBalanceQuery hook
-  // DATA FETCHED IS USED AS THE ALIAS BALANCE TO PREVENT REDEFINED ERROR
-  const { data: balance } = useGetBalanceQuery(id);
-  console.log(balance);
-  // DISPATCH getWalletBalance Action WITH THE FETCHED BALANCE TO BE STORED IN THE BALANCE STATE
-  dispatch(
-    getWalletBalance({
-      balance: balance?.balances[1]?.balance,
-      id: id,
-    })
-  );
+//   // GET WALLET BALANCE FROM balanceApi, PASS WALLET KEY TO THE useGetBalanceQuery RTK hook. THE balanceApi FETCHES THE WALLET BALANCE FROM BANTU API. THE RETURNED DATA IS DESTRUCTURED FROM THE useGetBalanceQuery hook
+//   // DATA FETCHED IS USED AS THE ALIAS BALANCE TO PREVENT REDEFINED ERROR
+//   const { data: balance } = useGetBalanceQuery(id);
+//   console.log(balance);
+//   // DISPATCH getWalletBalance Action WITH THE FETCHED BALANCE TO BE STORED IN THE BALANCE STATE
+//   dispatch(
+//     getWalletBalance({
+//       balance: balance?.balances[1]?.balance,
+//       id: id,
+//     })
+//   );
 
-  // PAYMENTS IN
-  const paymentIn = payments?.filter(
-    (payment) => payment.from !== id && payment.type == "payment"
-  );
+//   // PAYMENTS IN
+//   const paymentIn = payments?.filter(
+//     (payment) => payment.from !== id && payment.type == "payment"
+//   );
 
-  // PAYMENT OUT
-  const paymentOut = payments?.filter(
-    (payment) =>
-      payment.from == id ||
-      payment.asset_type == "native" ||
-      payment.asset_type == "create_account"
-  );
+//   // PAYMENT OUT
+//   const paymentOut = payments?.filter(
+//     (payment) =>
+//       payment.from == id ||
+//       payment.asset_type == "native" ||
+//       payment.asset_type == "create_account"
+//   );
 
-  return (
-    <TransactionsPageLayout>
-      <div>
-        <Header />
-        <div className='flex flex-row justify-between'>
-          <main className={style.main}>
-            <In paymentIn={paymentIn} />
-            <Out paymentOut={paymentOut} />
-          </main>
-        </div>
-      </div>
-    </TransactionsPageLayout>
-  );
-};
+//   return (
+//     <TransactionsPageLayout>
+//       <div>
+//         <Header />
+//         <div className='flex flex-row justify-between'>
+//           <main className={style.main}>
+//             <In paymentIn={paymentIn} />
+//             <Out paymentOut={paymentOut} />
+//           </main>
+//         </div>
+//       </div>
+//     </TransactionsPageLayout>
+//   );
+// };
 
-export default Wallet;
+// export default Wallet;
